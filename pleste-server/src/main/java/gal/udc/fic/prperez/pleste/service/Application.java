366d7638f8dad8fd0da444f3ad93e087f5a6dd91
Pablo R. P <p.rperez@udc.es>
2024-04-21 01:02:53 +0200

    Engadida definición de OpenAPI
    Queda pendente averiguar como metela baixo unha URL
    
    Signed-off-by: Pablo R. P <p.rperez@udc.es>

--- pleste-server/src/main/java/gal/udc/fic/prperez/pleste/service/Application.java
+++ pleste-server/src/main/java/gal/udc/fic/prperez/pleste/service/Application.java
@@ -1,13 +1,36 @@
 package gal.udc.fic.prperez.pleste.service;
 
-import org.springframework.boot.SpringApplication;
+import jakarta.ws.rs.ApplicationPath;
+import org.glassfish.jersey.servlet.ServletContainer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static gal.udc.fic.prperez.pleste.service.Application.BASE_URL;
+
+@ApplicationPath(BASE_URL)
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+	public final static String BASE_URL = "/api/v0";
+
 	public static void main(String[] args) {
 		new Application().configure(new SpringApplicationBuilder(Application.class)).run(args);
+	}
+
+	@Bean
+	public ServletRegistrationBean mainServlet(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new ServletContainer(), BASE_URL + "/*");
+
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("jersey.config.server.provider.packages", "gal.udc.fic.prperez.pleste.service");
+		registrationBean.setInitParameters(params);
+		registrationBean.setOrder(1);
+
+		return registrationBean;
 	}
 }
--- pleste-server/src/main/java/gal/udc/fic/prperez/pleste/service/JerseyConfig.java
+++ pleste-server/src/main/java/gal/udc/fic/prperez/pleste/service/JerseyConfig.java
@@ -1,11 +1,13 @@
 package gal.udc.fic.prperez.pleste.service;
 
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
 	public JerseyConfig() {
+		register(OpenApiResource.class);
 		register(TemplateResource.class);
 	}
 }
